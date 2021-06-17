0d6f3cc627d11897d649395f806699c30cd42e2f
Alexandre Cezar <alexandre_cezar@yahoo.com.br>
2021-06-17 13:00:20 -0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 # Author - Alexandre S. Cezar
 
-output "instance_docker_public_ip" {
-  value = google_compute_instance.vm-docker[count.index].network_interface.0.access_config.0.nat_ip
-}
+#output "instance_docker_public_ip" {
+#  value = google_compute_instance.vm-docker[count.index].network_interface.0.access_config.0.nat_ip
+#}
